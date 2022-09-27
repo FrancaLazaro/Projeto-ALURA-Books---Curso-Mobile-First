c185ad35e8e5193419d2ba19e9189eb039cbe770
Lázaro França <104153951+FrancaLazaro@users.noreply.github.com>
2022-09-27 16:57:53 -0300

    Carrossel criado com swiperjs

---
+++ AluraBooks/carrossel.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    }
+});
