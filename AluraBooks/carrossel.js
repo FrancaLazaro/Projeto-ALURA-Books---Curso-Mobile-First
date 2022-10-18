4cb6e85768c9f89af2019abe3bd33b7085f2677c
Lázaro França <lazmfa@gmail.com>
2022-10-18 06:45:06 -0300

    criando media query do carrossel

--- AluraBooks/carrossel.js
+++ AluraBooks/carrossel.js
@@ -1,10 +1,15 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
-    spaceBetween: 15,
+    spaceBetween: 50,
     loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-    }
+    },
 });
